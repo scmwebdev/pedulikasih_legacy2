1f6abe8e854f80c2a549f1f5e660cf1bd2eb9bef
enoa7 <gamal.ardi@gmail.com>
2016-02-25 19:15:18 +0700

    static misc

---
+++ static.indosiar.com/log-2016-02-10.php
@@ -0,0 +1,28 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-02-10 00:00:01 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 00:00:01 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 00:00:01 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
+ERROR - 2016-02-10 00:30:01 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 00:30:01 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 00:30:01 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
+ERROR - 2016-02-10 03:59:36 --> 404 Page Not Found --> robots.txt
+ERROR - 2016-02-10 03:59:36 --> Severity: Warning  --> chmod() [<a href='function.chmod'>function.chmod</a>]: Operation not permitted /usr/share/nginx/www/ci213/libraries/Log.php 106
+ERROR - 2016-02-10 06:00:01 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 06:00:01 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 06:00:02 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
+ERROR - 2016-02-10 06:30:01 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 06:30:01 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 06:30:01 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
+ERROR - 2016-02-10 12:00:01 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 12:00:01 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 12:00:01 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
+ERROR - 2016-02-10 12:30:01 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 12:30:01 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 12:30:01 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
+ERROR - 2016-02-10 18:00:01 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 18:00:01 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 18:00:01 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
+ERROR - 2016-02-10 18:30:02 --> Severity: Notice  --> Undefined index: REMOTE_ADDR /usr/share/nginx/www/ci213/core/Input.php 351
+ERROR - 2016-02-10 18:30:02 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /usr/share/nginx/www/ci213/core/Exceptions.php:185) /usr/share/nginx/www/ci213/libraries/Session.php 675
+ERROR - 2016-02-10 18:30:02 --> Severity: Warning  --> imap_open(): Couldn't open stream {webmail.indosiar.com:143/imap/novalidate-cert}INBOX /usr/share/nginx/www/cms.indosiar.com/app.cms/controllers/cron.php 67
