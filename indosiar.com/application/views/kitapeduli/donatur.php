287c621499c09821c94ab9a7ede42802d73eb4c9
enoa7 <gamal.ardi@gmail.com>
2016-02-29 14:38:08 +0700

    revert

--- indosiar.com/application/views/kitapeduli/donatur.php
+++ indosiar.com/application/views/kitapeduli/donatur.php
@@ -115,8 +115,8 @@ if ($que == 1) {
     $jenis=$this->session->userdata('jenis');
   }
   
-  $qword = mysqli_real_escape_string($db, $qword);
-  $jenis = mysqli_real_escape_string($db, $jenis);
+  $qword = mysqli_real_escape_string($qword);
+  $jenis = mysqli_real_escape_string($jenis);
 
   if ($jenis == "kota") {
     $sqltot = "select id from kitapeduli_bca where kategori='$kategori' and kota like '%$qword%'";
