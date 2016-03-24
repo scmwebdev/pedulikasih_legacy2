764e71a8fd284f302df2d511b648c86a4b266b3f
gamal <gamal.pratama@scm.co.id>
2016-03-24 13:32:40 +0700

    change URL_STATIC to indosiar.com:8081

--- indosiar.com/application/config/constants.php
+++ indosiar.com/application/config/constants.php
@@ -36,7 +36,8 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
-define('URL_STATIC', 'http://static.indosiar.com/');
+// define('URL_STATIC', 'http://static.indosiar.com/');
+define('URL_STATIC', 'http://indosiar.com:8080/');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
